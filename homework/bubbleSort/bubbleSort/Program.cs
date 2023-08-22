56eaab2fb59374618c1a768736ed64dd4b74c88b
guru9996 <51997337+guru9996@users.noreply.github.com>
2023-08-22 15:56:06 +0300

    BubbleSort and structure ready

---
+++ homework/bubbleSort/bubbleSort/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace bubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? s = Console.ReadLine();
+            List<int> numList = new List<int>();
+
+            string[] numsStr = s.Split(' ');
+            int num;
+            foreach (string? numStr in numsStr)
+            {
+                bool sucParse = int.TryParse(numStr, out num);
+                if (sucParse)
+                {
+                    numList.Add(num);
+                }
+            }
+            for (int i = 0; i < numList.Count; i++)
+            {
+                for (int j = 0; j < numList.Count - i - 1; j++)
+                {
+                    if (numList[j] > numList[j + 1])
+                    {
+                        int temp = numList[j];
+                        numList[j] = numList[j + 1];
+                        numList[j + 1] = temp;
+                    }
+                }
+            }
+            for(int i = 0;i < numList.Count;i++)
+            {
+                Console.Write(numList[i] + " ");
+            }
+        }
+    }
+}
